6e7f25d3eff8d61143918b092590f42e9db57864
Aaron Rietschlin <nemisis.82@gmail.com>
2014-03-26 18:34:46 -0400

    Added crashlytics to the timber.

--- app/src/main/java/com/asa/games/twentyfortyeight/AppClass.java
+++ app/src/main/java/com/asa/games/twentyfortyeight/AppClass.java
@@ -16,6 +16,34 @@ public class AppClass extends Application {
 
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
+        } else {
+            Timber.plant(new CrashReportingTree());
+        }
+    }
+
+    /**
+     * A tree which logs important information for crash reporting.
+     */
+    private static class CrashReportingTree extends Timber.HollowTree {
+        @Override
+        public void i(String message, Object... args) {
+            Crashlytics.log(String.format(message, args));
+        }
+
+        @Override
+        public void i(Throwable t, String message, Object... args) {
+            i(message, args); // Just add to the log.
+        }
+
+        @Override
+        public void e(String message, Object... args) {
+            i("ERROR: " + message, args); // Just add to the log.
+        }
+
+        @Override
+        public void e(Throwable t, String message, Object... args) {
+            e(message, args);
+            Crashlytics.logException(t);
         }
     }
 }
